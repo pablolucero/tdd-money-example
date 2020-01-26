eebe7609a8d3c701026b921d541237401e18f8ca
Pablo Lucero <plucero87@gmail.com>
2020-01-26 20:20:33 -0300

    decoupled test code from the existence of concrete subclasses by introducing factory methods

--- src/main/java/com/example/Franc.java
+++ src/main/java/com/example/Franc.java
@@ -6,7 +6,7 @@ public class Franc extends Money {
         super(amount);
     }
 
-    public Franc times(int multiplier) {
+    public Money times(int multiplier) {
         return new Franc(amount * multiplier);
     }
 
--- src/main/java/com/example/Money.java
+++ src/main/java/com/example/Money.java
@@ -1,11 +1,19 @@
 package com.example;
 
-public class Money {
+public abstract class Money {
 
     protected int amount;
 
     public Money(int amount) {
         this.amount = amount;
+    }
+
+    public static Money dollar(int amount) {
+        return new Dollar(amount);
+    }
+
+    public static Money franc(int amount) {
+        return new Franc(amount);
     }
 
     @Override
@@ -14,4 +22,6 @@ public class Money {
         return amount == money.amount
                 && getClass().equals(money.getClass());
     }
+
+    abstract Money times(int multiplier);
 }
