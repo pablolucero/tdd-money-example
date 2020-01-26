227677964a6159b17d027331db0c99eddba26411
Pablo Lucero <plucero87@gmail.com>
2020-01-26 05:27:00 -0300

    remove side effect in times(multiplier) method

--- src/main/java/com/example/Dollar.java
+++ src/main/java/com/example/Dollar.java
@@ -8,7 +8,7 @@ public class Dollar {
         this.amount = amount;
     }
 
-    public void times(int multiplier) {
-        amount *= multiplier;
+    public Dollar times(int multiplier) {
+        return new Dollar(amount * multiplier);
     }
 }
