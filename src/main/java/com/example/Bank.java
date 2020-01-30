e409982046d2189491a3b970c454edcee9b65ec5
Pablo Lucero <plucero87@gmail.com>
2020-01-30 01:03:18 -0300

    make testSimpleAddition() fake pass with bank abstraction

---
+++ src/main/java/com/example/Bank.java
@@ -0,0 +1,7 @@
+package com.example;
+
+public class Bank {
+    public Money reduce(Expression source, String to) {
+        return Money.dollar(10);
+    }
+}
--- src/main/java/com/example/Money.java
+++ src/main/java/com/example/Money.java
@@ -1,6 +1,6 @@
 package com.example;
 
-public class Money {
+public class Money implements Expression {
 
     protected int amount;
     protected String currency;
