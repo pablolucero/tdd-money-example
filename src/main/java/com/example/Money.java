06cb18f8fb2bce772995690706b3b1e1b1cebc23
Pablo Lucero <plucero87@gmail.com>
2020-01-30 00:47:42 -0300

    add testSimpleAddition() without bank abstraction

--- src/main/java/com/example/Money.java
+++ src/main/java/com/example/Money.java
@@ -37,4 +37,8 @@ public class Money {
     public Money times(int multiplier) {
         return new Money(amount * multiplier, currency);
     }
+
+    public Money plus(Money addend) {
+        return new Money(amount + addend.amount, currency);
+    }
 }
--- src/test/java/com/example/MoneyTest.java
+++ src/test/java/com/example/MoneyTest.java
@@ -27,4 +27,10 @@ class MoneyTest {
         assertEquals("CHF", Money.franc(1).currency());
     }
 
+    @Test
+    void testSimpleAddition() {
+        Money sum = Money.dollar(5).plus(Money.dollar(5));
+        assertEquals(Money.dollar(10), sum);
+    }
+
 }
