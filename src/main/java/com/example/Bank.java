d61a650e4866ed59a7cd87bfcca382c7b3c8c249
Pablo Lucero <plucero87@gmail.com>
2020-01-30 22:54:32 -0300

    add testReduceMoneyDifferentCurrency()

--- src/main/java/com/example/Bank.java
+++ src/main/java/com/example/Bank.java
@@ -2,6 +2,16 @@ package com.example;
 
 public class Bank {
     public Money reduce(Expression source, String to) {
-        return source.reduce(to);
+        return source.reduce(this, to);
+    }
+
+    public void addRate(String from, String to, int rate) {
+
+    }
+
+    public int rate(String from, String to) {
+        return (from.equals("CHF") && to.equals("USD"))
+                ? 2
+                : 1;
     }
 }
