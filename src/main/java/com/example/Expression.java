d7478113e0a99f40e630a6ce261657c686508741
Pablo Lucero <plucero87@gmail.com>
2020-01-30 23:55:11 -0300

    add testMixedAddition()

--- src/main/java/com/example/Expression.java
+++ src/main/java/com/example/Expression.java
@@ -2,4 +2,6 @@ package com.example;
 
 public interface Expression {
     Money reduce(Bank bank, String to);
+
+    Expression plus(Expression addend);
 }
--- src/test/java/com/example/MoneyTest.java
+++ src/test/java/com/example/MoneyTest.java
@@ -72,4 +72,14 @@ class MoneyTest {
     void testIdentityRate() {
         assertEquals(1, new Bank().rate("USD", "USD"));
     }
+
+    @Test
+    void testMixedAddition() {
+        Expression fiveBucks = Money.dollar(5);
+        Expression tenFrancs = Money.franc(10);
+        Bank bank = new Bank();
+        bank.addRate("CHF", "USD", 2);
+        Money result = bank.reduce(fiveBucks.plus(tenFrancs), "USD");
+        assertEquals(Money.dollar(10), result);
+    }
 }
